0376cc1aecff93e22acb05763c6896db61280895
YumaIshida00 <yu_basket11@icloud.com>
2022-09-19 16:12:29 +0900

    微修正

--- go/main.go
+++ go/main.go
@@ -1213,7 +1213,7 @@ func postIsuCondition(c echo.Context) error {
 		})
 	}
 
-	_, err = tx.Exec(
+	_, err = tx.NamedExec(
 		"INSERT INTO `isu_condition`"+
 			"	(`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`)"+
 			"	VALUES (:jia_isu_uuid, :timestamp, :is_sitting, :condition, :message)",
